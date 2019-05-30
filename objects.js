0dc2c49f9b4f43554b6d24623e02f02c2da2b37e
Anh Nguyen <anhnguyenis@gmail.com>
2019-05-30 23:56:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artistName: "songTitle"};
 
 function updatePlaylist(playlist,artistName,songTitle) {
-  Object.assign ({},{artistNam:"songTitle"})
+  Object.assign ({artistName:"songTitle"})
 }
