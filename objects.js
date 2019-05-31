b21f8efee76e9b86e07f801e300e8b108e72e6bd
Anh Nguyen <anhnguyenis@gmail.com>
2019-05-31 00:03:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist = {artistName: "songTitle"};
 
 function updatePlaylist(playlist,artistName,songTitle) {
-  return Object.assign (playlist,{[artistName]:"songTitle"})
+  return Object.assign (playlist,{[artistName]:"songTitle"});
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  return delete.artistName;
+  return delete playlist.artistName;
 }
